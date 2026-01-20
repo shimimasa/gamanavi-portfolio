ae3501871a60eb418d734f8631dca9bef27477d8
shimizu <teacher.shimizu14@gmail.com>
2026-01-20 11:27:57 +0900

    chore: add Tailwind CSS and PostCSS configuration for styling

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+      "./src/app/**/*.{js,jsx,ts,tsx}",
+      "./src/components/**/*.{js,jsx,ts,tsx}",
+      "./src/**/*.{js,jsx,ts,tsx}",
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  };
+  
